0923a9a2daf87739bbcab816a9abd76663f20140
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-07-16 09:54:52 +0900

    feat: コード調整

--- src/locustfile.py
+++ src/locustfile.py
@@ -1,6 +1,9 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, constant_pacing
 
 class HelloWorldUser(HttpUser):
+    #  1タスク = 1秒間に1回リクエストを送信する
+    wait_time = constant_pacing(1)
+
     @task
     def hello_world(self):
         self.client.get("/hello-world")
